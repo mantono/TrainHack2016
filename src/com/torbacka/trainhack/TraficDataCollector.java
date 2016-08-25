756b346136705bc701ea6ea7369593da6ec7763d
LAPTOP\Daniel Törbacka <daniel.torbacka@gmail.com>
2016-08-25 14:56:26 +0200

    added lookup method

--- src/com/torbacka/trainhack/TraficDataCollector.java
+++ src/com/torbacka/trainhack/TraficDataCollector.java
@@ -3,7 +3,6 @@ package com.torbacka.trainhack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.text.ParseException;
@@ -20,16 +19,12 @@ import com.torbacka.trainhack.model.Rss;
  * Created by Taco on 2016-08-25.
  */
 public class TraficDataCollector {
-    private static final String BASE_URL = "https://api.resrobot.se/v2/departureBoard?key=15708608-3e9f-46fe-a996-4d0af82de95e&id=740000001&maxJourneys=50&passlist=0&format=json";
+    private static final String BASE_URL = "https://api.resrobot.se/v2/departureBoard?key=15708608-3e9f-46fe-a996-4d0af82de95e&maxJourneys=50&passlist=0&format=json&id=";
+    private static final String LOOKUP_URL = "https://api.resrobot.se/v2/location.name?key=ebffa80d-b41b-4469-96f4-31d90c73f790&format=xml&&input="
     private static final String KEY = "15708608-3e9f-46fe-a996-4d0af82de95e";
-    private static final String BASE_RSS = "<?xml version=\"1.0\"?>\n" +
-                    "<rss version=\"2.0\">\n" +
-                    "<channel>" +
-                    "<title>Avgångstavlan</title>" +
-                    "<link>http://localhost/avgangstavlan?locationId=7400000001</link>\n" ;
 
     public static Rss getTraficDataAsRss(int stopId) throws IOException, ParseException {
-        URL oracle = new URL(BASE_URL);
+        URL oracle = new URL(BASE_URL + stopId);
         URLConnection yc = oracle.openConnection();
         BufferedReader in = new BufferedReader(new InputStreamReader(
                         yc.getInputStream()));
@@ -44,7 +39,7 @@ public class TraficDataCollector {
         JsonNode rootNode = mapper.readTree(output.toString());
         JsonNode departures = rootNode.get("Departure");
         List<Item> items = new ArrayList<Item>();
-        if(departures.isArray()) {
+        if (departures.isArray()) {
             for (JsonNode node : departures) {
                 System.out.println(node.get("stopid").asText());
                 final String title = node.get("direction").asText();
@@ -53,12 +48,13 @@ public class TraficDataCollector {
                 final String time = node.get("time").asText();
                 final JsonNode url = node.get("Product").get("operatorUrl");
                 URL guid;
-                if(url != null)
-                	guid = new URL(node.get("Product").get("operatorUrl").asText());
-                else
-                	guid = new URL("http://samtrafiken.se");
+                if (url != null) {
+                    guid = new URL(node.get("Product").get("operatorUrl").asText());
+                } else {
+                    guid = new URL("http://samtrafiken.se");
+                }
                 final Item item = new Item(guid, date, time, title, desc);
-                items.add(item);                
+                items.add(item);
             }
         }
 
@@ -71,55 +67,75 @@ public class TraficDataCollector {
         return rss;
     }
 
-	public static Rss getTraficDataAsRss(int stopStart, int stopDest) throws IOException, ParseException {
-		
-		final String destinatioName = getDesitnaNameForId(stopDest);
-	     URL oracle = new URL(BASE_URL);
-	        URLConnection yc = oracle.openConnection();
-	        BufferedReader in = new BufferedReader(new InputStreamReader(
-	                        yc.getInputStream()));
-	        String inputLine;
-	        StringBuffer output = new StringBuffer();
+    public static Rss getTraficDataAsRss(int stopStart, int stopDest) throws IOException, ParseException {
 
-	        while ((inputLine = in.readLine()) != null) {
-	            output.append(inputLine);
-	        }
+        final String destinatioName = getDesitnaNameForId(stopDest);
+        URL oracle = new URL(BASE_URL + stopStart);
+        URLConnection yc = oracle.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                        yc.getInputStream()));
+        String inputLine;
+        StringBuffer output = new StringBuffer();
 
-	        ObjectMapper mapper = new ObjectMapper();
-	        JsonNode rootNode = mapper.readTree(output.toString());
-	        JsonNode departures = rootNode.get("Departure");
-	        List<Item> items = new ArrayList<Item>();
-	        if(departures.isArray()) {
-	            for (JsonNode node : departures) {
-	                System.out.println(node.get("stopid").asText());
-	                final String title = node.get("direction").asText();
-	                if(!title.equals(destinatioName))
-	                	continue;
-	                final String desc = node.get("name").asText();
-	                final String date = node.get("date").asText();
-	                final String time = node.get("time").asText();
-	                final JsonNode url = node.get("Product").get("operatorUrl");
-	                URL guid;
-	                if(url != null)
-	                	guid = new URL(node.get("Product").get("operatorUrl").asText());
-	                else
-	                	guid = new URL("http://samtrafiken.se");
-	                final Item item = new Item(guid, date, time, title, desc);
-	                items.add(item);                
-	            }
-	        }
+        while ((inputLine = in.readLine()) != null) {
+            output.append(inputLine);
+        }
 
-	        //System.out.println(departure.size());
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(output.toString());
+        JsonNode departures = rootNode.get("Departure");
+        List<Item> items = new ArrayList<Item>();
+        if (departures.isArray()) {
+            for (JsonNode node : departures) {
+                System.out.println(node.get("stopid").asText());
+                final String title = node.get("direction").asText();
+                if (!title.equals(destinatioName)) {
+                    continue;
+                }
+                final String desc = node.get("name").asText();
+                final String date = node.get("date").asText();
+                final String time = node.get("time").asText();
+                final JsonNode url = node.get("Product").get("operatorUrl");
+                URL guid;
+                if (url != null) {
+                    guid = new URL(node.get("Product").get("operatorUrl").asText());
+                } else {
+                    guid = new URL("http://samtrafiken.se");
+                }
+                final Item item = new Item(guid, date, time, title, desc);
+                items.add(item);
+            }
+        }
 
-	        final Channel channel = new Channel("En fin titel", "http://google.se", items);
-	        Rss rss = new Rss("2.0", channel);
+        //System.out.println(departure.size());
 
-	        in.close();
-	        return rss;
-	}
+        final Channel channel = new Channel("En fin titel", "http://google.se", items);
+        Rss rss = new Rss("2.0", channel);
 
-	private static String getDesitnaNameForId(int stopDest) {
-		// TODO här taco!
-		return null;
-	}
+        in.close();
+        return rss;
+    }
+
+    private static int getDesitnaNameForId(String direction) {
+        URL oracle = new URL(LOOKUP_URL + stopStart);
+        URLConnection yc = oracle.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                        yc.getInputStream()));
+
+        String inputLine;
+        StringBuffer output = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            output.append(inputLine);
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = mapper.readTree(output.toString());
+        JsonNode stopLocation = rootNode.get("StopLocation");
+
+        int stopDest = stopLocation.get("name").asText();
+
+
+        return stopDest;
+    }
 }
